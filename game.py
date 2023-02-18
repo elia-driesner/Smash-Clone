f1931843bc97779031d9968f8761c09748494fa5
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2023-02-18 18:09:45 +0100

    fixed bugs

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
 import pygame
 import sys
 
+from scripts.player import Player
+
 class Game():
     def __init__(self):
         self.width, self.height = 960, 540
         self.window = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Platformer')
         self.run = True
+        
+        self.player = Player(100, 100)
         
     def events(self):
         for event in pygame.event.get():
--- scripts/player.py
+++ scripts/player.py
@@ -1,18 +1,18 @@
-import pygame
+import pygame, os
 
 class Player():
     def __init__(self, x, y):
         self.x, self.y = x, y
-        self.image = pygame.transform.scale(pygame.image.load(os.path.join('../assets/images/player/idle/', 'player-idle.png')), (16, 16))
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join('assets/images/player/idle/', 'player-idle.png')), (16, 16))
         self.rect = self.image.get_rect()
         self.speed = 5
         
     def move(self):
         keys = pygame.key.get_pressed()
-            if keys[pygame.K_a]:
-                self.x -= self.speed
-            elif keys[pygame.K_d]:
-                self.x += self.speed
+        if keys[pygame.K_a]:
+            self.x -= self.speed
+        elif keys[pygame.K_d]:
+            self.x += self.speed
                 
         self.rect.x = self.x
         self.rect.y = self.y
