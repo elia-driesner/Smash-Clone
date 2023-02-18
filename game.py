9bfbe703ff6ee10cc0ac416e0075f4ab420c9d52
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2023-02-18 23:26:18 +0100

    walking friction, jumping physics, double jump

--- game.py
+++ game.py
@@ -1,15 +1,28 @@
 import pygame, sys
+import time
 
 from scripts.player import Player
 
+pygame.init()
+
 class Game():
     def __init__(self):
-        self.width, self.height = 960, 540
-        self.window = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption('Platformer')
+        # self.width, self.height = 960, 540
+        self.width, self.height = 1920, 1080
+        self.window = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN)
+        pygame.display.set_caption('Smash')
         self.run = True
+        self.MAX_FPS = 60
+        self.dt = 0
+        self.last_time = time.time()
         
-        self.player = Player(100, 100)
+        self.player = Player(100, 100, 40, 40)
+        self.clock = pygame.time.Clock()
+        
+    def calculate_dt(self):
+        self.dt = time.time() - self.last_time
+        self.dt *= 60
+
         
     def events(self):
         for event in pygame.event.get():
@@ -17,10 +30,22 @@ class Game():
                 pygame.quit()
                 self.run = False
                 sys.exit(0)
+        
+    def collision(self):
+        if self.player.rect.y >= 600:
+            self.player.on_ground = True
                 
     def loop(self):
         while self.run:
+            self.clock.tick(self.MAX_FPS)
+            self.calculate_dt()
+            self.window.fill((0, 0, 0))
             self.events()
+            self.collision()
+            self.player.update(self.window, self.dt)
+            self.last_time = time.time()
+            
+            pygame.display.update()
                 
 
 game = Game()
--- scripts/player.py
+++ scripts/player.py
@@ -1,18 +1,71 @@
-import pygame, os
+import pygame, os, time
 
 class Player():
-    def __init__(self, x, y):
+    def __init__(self, x, y, width, height):
         self.x, self.y = x, y
-        self.image = pygame.transform.scale(pygame.image.load(os.path.join('assets/images/player/idle/', 'player-idle.png')), (16, 16))
+        self.width, self.height = width, height
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join('assets/images/player/idle/', 'player-idle.png')), (self.width, self.height))
         self.rect = self.image.get_rect()
-        self.speed = 5
         
-    def move(self):
+        self.is_jumping, self.on_ground = False, False
+        self.speed = 2
+        self.double_jump = True
+        self.last_jump = time.time()
+        self.gravity, self.friction = .32, -.13
+        self.position, self.velocity = pygame.math.Vector2(0, 0), pygame.math.Vector2(0, 0)
+        self.acceleration = pygame.math.Vector2(0, self.gravity)
+
+        
+    def move(self, dt):
+        self.acceleration.x = 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            self.x -= self.speed
+            self.acceleration.x -= self.speed
         elif keys[pygame.K_d]:
-            self.x += self.speed
+            self.acceleration.x += self.speed
+        if keys[pygame.K_SPACE]:
+            self.jump()
+            
+        self.acceleration.x += self.velocity.x * self.friction
+        self.velocity.x += self.acceleration.x * dt
+        self.limit_velocity(4)
+        self.position.x += self.velocity.x * dt + (self.acceleration.x * .5) * (dt * dt)
+        self.x = self.position.x
+        
+        self.velocity.y += self.acceleration.y * dt
+        if self.velocity.y > 7: self.velocity.y = 7
+        if self.on_ground: 
+            self.velocity.y = 0
+        else:
+            self.position.y += self.velocity.y * dt + (self.acceleration.y * .5) * (dt * dt)
+        self.y = self.position.y
                 
         self.rect.x = self.x
         self.rect.y = self.y
+    
+    def jump(self):
+        if self.on_ground:
+            self.last_jump = time.time()
+            self.double_jump = True
+            self.is_jumping = True
+            self.velocity.y -= 12
+            self.on_ground = False
+        elif self.double_jump and self.on_ground == False and time.time() - self.last_jump > 0.3:
+            self.double_jump = False
+            self.is_jumping = True
+            self.velocity.y -= 12
+            self.on_ground = False
+        if self.velocity.y <= -12.5:
+            self.velocity.y = -12
+        
+    
+    def limit_velocity(self, max_vel):
+        min(-max_vel, max(self.velocity.x, max_vel))
+        if abs(self.velocity.x) < .01: self.velocity.x = 0
+
+    def draw(self, window):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+        
+    def update(self, window, dt):
+        self.move(dt)
+        self.draw(window)
