e47a6bfa36af56e4b99973b85e619a818408c7c1
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2023-02-18 17:25:00 +0100

    added pygame basics and createt folder structure

--- game.py
+++ game.py
@@ -1,1 +1,23 @@
 import pygame
+import sys
+
+class Game():
+    def __init__(self):
+        self.width, self.height = 960, 540
+        self.window = pygame.display.set_mode((self.width, self.height))
+        self.run = True
+        
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                self.run = False
+                sys.exit(0)
+                
+    def loop(self):
+        while self.run:
+            self.events()
+                
+
+game = Game()
+game.loop()
